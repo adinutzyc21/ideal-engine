6f5d116df9da20d66f2f42ede9626b0157e52376
Adina Stoica <adina.stoica@gmail.com>
2017-05-05 23:04:12 -0500

    Deleted tutorial pages

--- compare-app/imports/ui/pages/One.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';  // eslint-disable-line no-unused-vars
-
-/**
- * A simple component to render when we visit the index route / of our application.
- */
-export const One = () => <div className='table-container table-container-no-data'>
-    <h3>Page One</h3>
-  </div>;
--- compare-app/imports/ui/pages/Two.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';  // eslint-disable-line no-unused-vars
-
-/**
- * A simple component to render when we visit the index route / of our application.
- */
-export const Two = () => <div className='table-container table-container-no-data'>
-      <h3>Page Two</h3>
-    </div>;
